8e41730e8f7edd5762335bd2bf58ccf104cccabb
sundarad <sundar.adhikari@bandipurcablecar.com.np>
2022-09-13 10:53:38 +0545

    slider

--- wp-content/themes/custom_theme/header.php
+++ wp-content/themes/custom_theme/header.php
@@ -16,10 +16,18 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+    <link href="<?php echo get_template_directory_uri();?>/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/bootstrap/css/bootstrap.min.css" type="text/css">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/front-assets/bootstrap-icons/bootstrap-icons.css" type="text/css">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/front-assets/css/style.css" type="text/css">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,400,500,600,700" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="<?php echo get_template_directory_uri();?>/assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="<?php echo get_template_directory_uri();?>/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo get_template_directory_uri();?>/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="<?php echo get_template_directory_uri();?>/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="<?php echo get_template_directory_uri();?>/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link href="<?php echo get_template_directory_uri();?>/assets/css/style.css" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 
@@ -58,7 +66,7 @@
                         <div class="widget-left clearfix">
                             <div class="info-widget">
                                 <div class="info-widget-content">
-                                    <img src="assets/front-assets/images/info-left-arrow.png" alt="">
+
                                     <p>For Emergency call</p><h2>+977 9806555607</h2> </div>
                             </div>
                         </div>
@@ -78,7 +86,7 @@
                     <div class="col-lg-3">
                         <div class="widget-right clearfix">
                             <div class="info-widget">
-                                <img src="assets/front-assets/images/info-right-arrow.png" alt="">
+
                                 <div class="info-widget-content"><p>Request an</p> <h2>Appointment</h2> </div>
                             </div>
                         </div>
