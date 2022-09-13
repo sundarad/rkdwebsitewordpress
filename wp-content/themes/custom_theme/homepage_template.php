102d7e5b3325cc82027acd8ef667925447bfdb38
sundarad <sundar.adhikari@bandipurcablecar.com.np>
2022-09-13 11:04:00 +0545

    Home Page template

--- wp-content/themes/custom_theme/homepage_template.php
+++ wp-content/themes/custom_theme/homepage_template.php
@@ -114,41 +114,41 @@
         </div>
     </section><!-- End About Section -->
 
-    <!-- ======= About Section ======= -->
-    <section id="about" class="about">
-
-        <div class="container" data-aos="fade-up">
-            <div class="row">
-
-                <div class="col-lg-5 col-md-6">
-                    <div class="about-img" data-aos="fade-right" data-aos-delay="100">
-                        <img src="assets/img/about-img.jpg" alt="">
-                    </div>
-                </div>
-
-                <div class="col-lg-7 col-md-6">
-                    <div class="about-content" data-aos="fade-left" data-aos-delay="100">
-                        <h2>Who we are</h2>
-                        <h3>We Work With You To Address Your Most Critical Business Priorities</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <p>Aut dolor id. Sint aliquam consequatur ex ex labore. Et quis qui dolor nulla dolores neque. Aspernatur consectetur omnis numquam quaerat. Sed fugiat nisi. Officiis veniam molestiae. Et vel ut quidem alias veritatis repudiandae ut fugit. Est ut eligendi aspernatur nulla voluptates veniam iusto vel quisquam. Fugit ut maxime incidunt accusantium totam repellendus eum error. Et repudiandae eum iste qui et ut ab alias.</p>
-                        <ul>
-                            <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-                            <li><i class="bi bi-check-circle"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-                            <li><i class="bi bi-check-circle"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-                            <li><i class="bi bi-check-circle"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-                            <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
-                        </ul>
-                        <h3><strong>9806555609</strong></h3>
-                        <p>Call to ask any question</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </section><!-- End About Section -->
-
-    <!-- ======= Services Section ======= -->
+<!--    <!-- ======= About Section ======= -->-->
+<!--    <section id="about" class="about">-->
+<!---->
+<!--        <div class="container" data-aos="fade-up">-->
+<!--            <div class="row">-->
+<!---->
+<!--                <div class="col-lg-5 col-md-6">-->
+<!--                    <div class="about-img" data-aos="fade-right" data-aos-delay="100">-->
+<!--                        <img src="assets/img/about-img.jpg" alt="">-->
+<!--                    </div>-->
+<!--                </div>-->
+<!---->
+<!--                <div class="col-lg-7 col-md-6">-->
+<!--                    <div class="about-content" data-aos="fade-left" data-aos-delay="100">-->
+<!--                        <h2>Who we are</h2>-->
+<!--                        <h3>We Work With You To Address Your Most Critical Business Priorities</h3>-->
+<!--                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>-->
+<!--                        <p>Aut dolor id. Sint aliquam consequatur ex ex labore. Et quis qui dolor nulla dolores neque. Aspernatur consectetur omnis numquam quaerat. Sed fugiat nisi. Officiis veniam molestiae. Et vel ut quidem alias veritatis repudiandae ut fugit. Est ut eligendi aspernatur nulla voluptates veniam iusto vel quisquam. Fugit ut maxime incidunt accusantium totam repellendus eum error. Et repudiandae eum iste qui et ut ab alias.</p>-->
+<!--                        <ul>-->
+<!--                            <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>-->
+<!--                            <li><i class="bi bi-check-circle"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>-->
+<!--                            <li><i class="bi bi-check-circle"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>-->
+<!--                            <li><i class="bi bi-check-circle"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>-->
+<!--                            <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>-->
+<!--                        </ul>-->
+<!--                        <h3><strong>9806555609</strong></h3>-->
+<!--                        <p>Call to ask any question</p>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!---->
+<!--    </section><!-- End About Section -->-->
+<!---->
+<!--    <!-- ======= Services Section ======= -->-->
     <section id="services" class="services section-bg">
         <div class="container" data-aos="fade-up">
 
