1fef89bf1176b74d946c9784ae54177c818b1e15
sundarad <sundar.adhikari@bandipurcablecar.com.np>
2022-09-22 13:59:15 +0545

    Dynamic INvestment By

--- wp-content/themes/custom_theme/homepage_template.php
+++ wp-content/themes/custom_theme/homepage_template.php
@@ -543,92 +543,48 @@ if ($the_query->have_posts()) : ?>
                         </ul>
                     </div>
                 </div>
+                <?php $args = array(
+                    'post_type' => 'post',
+                    'category_name' => 'investment-portfolio',
+                    'posts_per_page' => 3,
+                    'orderby' => 'date',
+                    'order' => 'DESC'
+                );
 
-                <div class="row gy-4 portfolio-container" data-aos="fade-up" data-aos-delay="200">
+                $the_query = new WP_Query($args);
+                if ($the_query->have_posts()) : ?>
+                    <section id="services" class="services one">
+                        <div class="container">
+                            <div class="row gy-5">
+                                <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+                                <div class="row gy-4 portfolio-container" data-aos="fade-up" data-aos-delay="200">
+                                <?php
 
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-                        <div class="portfolio-wrap">
-                            <img src="assets/img/portfolio/app1.jpg" class="img-fluid" alt="">
-                            <div class="portfolio-info">
-                                <h4>App 1</h4>
-                                <p>App</p>
-                                <div class="portfolio-links">
-                                    <a href="assets/img/portfolio/app1.jpg" data-gallery="portfolioGallery" class="portfokio-lightbox" title="App 1"><i class="bi bi-plus"></i></a>
-                                    <a href="portfolio-details.html" title="More Details"><i class="bi bi-link"></i></a>
-                                </div>
+                                $portfolio_image = get_the_post_thumbnail_url(get_the_ID());
+                                ?>
+                                    <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+                                        <div class="portfolio-wrap">
+                                            <?php the_post_thumbnail(
+                                                array( 'class' => 'img-fluid' )
+                                            ); ?>
+                                            <div class="portfolio-info">
+                                                <h4><?php the_title(); ?></h4>
+                                                <p><?php get_the_content();?></p>
+                                                <div class="portfolio-links">
+                                                    <a href="<?php echo $portfolio_image?>" data-gallery="portfolioGallery" class="portfokio-lightbox" title="App 1"><i class="bi bi-plus"></i></a>
+                                                    <a href="<?php the_permalink(); ?>" title="More Details"><i class="bi bi-link"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                <?php endwhile; ?>
                             </div>
                         </div>
-                    </div>
+                    </section>
+                <?php endif; ?>
 
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-                        <div class="portfolio-wrap">
-                            <img src="assets/img/portfolio/app2.jpg" class="img-fluid" alt="">
-                            <div class="portfolio-info">
-                                <h4>Web 3</h4>
-                                <p>Web</p>
-                                <div class="portfolio-links">
-                                    <a href="assets/img/portfolio/app2.jpg" data-gallery="portfolioGallery" class="portfokio-lightbox" title="Web 3"><i class="bi bi-plus"></i></a>
-                                    <a href="portfolio-details.html" title="More Details"><i class="bi bi-link"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
 
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-                        <div class="portfolio-wrap">
-                            <img src="assets/img/portfolio/app3.jpg" class="img-fluid" alt="">
-                            <div class="portfolio-info">
-                                <h4>App 2</h4>
-                                <p>App</p>
-                                <div class="portfolio-links">
-                                    <a href="assets/img/portfolio/app3.jpg" data-gallery="portfolioGallery" class="portfokio-lightbox" title="App 2"><i class="bi bi-plus"></i></a>
-                                    <a href="portfolio-details.html" title="More Details"><i class="bi bi-link"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-                        <div class="portfolio-wrap">
-                            <img src="assets/img/portfolio/app2.jpg" class="img-fluid" alt="">
-                            <div class="portfolio-info">
-                                <h4>Card 2</h4>
-                                <p>Card</p>
-                                <div class="portfolio-links">
-                                    <a href="assets/img/portfolio/app2.jpg" data-gallery="portfolioGallery" class="portfokio-lightbox" title="Card 2"><i class="bi bi-plus"></i></a>
-                                    <a href="portfolio-details.html" title="More Details"><i class="bi bi-link"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-                        <div class="portfolio-wrap">
-                            <img src="assets/img/portfolio/app3.jpg" class="img-fluid" alt="">
-                            <div class="portfolio-info">
-                                <h4>Web 2</h4>
-                                <p>Web</p>
-                                <div class="portfolio-links">
-                                    <a href="assets/img/portfolio/app3.jpg" data-gallery="portfolioGallery" class="portfokio-lightbox" title="Web 2"><i class="bi bi-plus"></i></a>
-                                    <a href="portfolio-details.html" title="More Details"><i class="bi bi-link"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-                        <div class="portfolio-wrap">
-                            <img src="assets/img/portfolio/app1.jpg" class="img-fluid" alt="">
-                            <div class="portfolio-info">
-                                <h4>App 3</h4>
-                                <p>App</p>
-                                <div class="portfolio-links">
-                                    <a href="assets/img/portfolio/app1.jpg" data-gallery="portfolioGallery" class="portfokio-lightbox" title="App 3"><i class="bi bi-plus"></i></a>
-                                    <a href="portfolio-details.html" title="More Details"><i class="bi bi-link"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 </div>
 
             </div>
