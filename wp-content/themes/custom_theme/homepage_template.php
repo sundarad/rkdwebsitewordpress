532657e1a4fe2b8f627a64edf4997eca63e70a82
binisha <sherchanbinisha5@gmail.com>
2022-09-22 13:34:06 +0545

    Fix css

--- wp-content/themes/custom_theme/homepage_template.php
+++ wp-content/themes/custom_theme/homepage_template.php
@@ -134,11 +134,8 @@ if ($the_query->have_posts()) : ?>
                         <div class="about-content" data-aos="fade-left" data-aos-delay="100">
 
                             <h2><?php echo $title_who_we_are;?></h2>
-<!--                             --><?php //echo $content_who_we_are;?>
-
-                            <ul>
-                                <li> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-                            </ul>
+                             <?php echo $content_who_we_are;?>
+                            
                             </div>
                         </div>
                     </div>
