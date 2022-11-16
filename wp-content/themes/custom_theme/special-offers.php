2fd3bda9b4d60c63fccfbf262a18196a9e935990
binisha <sherchanbinisha5@gmail.com>
2022-11-16 15:01:37 +0545

    Add offer page

---
+++ wp-content/themes/custom_theme/special-offers.php
@@ -0,0 +1,129 @@
+<?php
+/* Template Name: special-offers*/
+?>
+<?php get_header();?>
+<!-- ======= Breadcrumbs ======= -->
+<!-- <section id="breadcrumbs" class="breadcrumbs">
+    <div class="container">
+    <h2>Special Offers</h2>
+        <ol>
+            <li><a href="/index.php"> <i class="bi bi-house-fill"></i> Home</a></li>
+            <li>Special Offers</li>
+        </ol>
+
+    </div>
+</section> -->
+<!-- End Breadcrumbs -->
+
+<!-- EVENT START -->
+<section id="event-banner">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col xs-12 text-center">
+                 <h2>ILAMI Scheme- RKD Holdings Limited (RKD) in Collab with Bizbazar</h2>
+                 <p class="text-center">RKD Holdings Limited in collaboration with Bizbazar Limited has released ILAMI Scheme.</p>
+                 <div class="d-inline-block text-center more-btn  animate__animated animate__fadeInUp scrollto">
+                    <a href="/contact-us-2/" class="btn-get-started scrollto">Contact us</a>
+                    <a href="#youtube-vid" class="glightbox btn-watch-video d-flex align-items-center mt-3 ms-3 position-relative" target="_blank"><i class="bi bi-play-circle"></i><span >All Videos</span></a>
+                 </div>
+            </div>
+             
+        </div>
+    </div>
+</section>
+<!-- EVENT END -->
+<section id="inner-event">
+    <div class="container">
+      <div class="row align-items-center mb-50">
+                 <div class="col-lg-6">
+                         <img src="<?php echo get_template_directory_uri();?>/assets/img/ilami-program.jpg" width="100%" height="700px"  alt="" class="border-radius-15 mb-md-3 mb-lg-0 mb-sm-4" style="border-radius:1rem;">
+                 </div>
+                 <div class="col-lg-6">
+                     <div class="ps-3">
+                        <h2 class="mb-30">RKD Special Offers</h2>
+                        <p class="mb-25">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate id est laborum.</p>
+                        <p class="mb-50">Ius ferri velit sanctus cu, sed at soleat accusata. Dictas prompta et Ut placerat legendos interpre.Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante Etiam sit amet orci eget. Quis commodo odio aenean sed adipiscing. Turpis massa tincidunt dui ut ornare lectus. Auctor elit sed vulputate mi sit amet. Commodo consequat. Duis aute irure dolor in reprehenderit in voluptate id est laborum.</p>
+      
+                        <div class="row d-flex align-items-center">
+      <div class="col-1 d-flex align-items-center justify-content-center">
+         <a role="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <div class="carousel-nav-icon"> <i class="bi bi-arrow-left"></i> </div>
+         </a>
+      </div>
+      <div class="col-10">
+         <!--Start carousel-->
+         <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+
+            <div class="carousel-inner">
+               <div class="carousel-item active">
+                  <div class="row">
+                     <div class="col-12 col-md d-flex align-items-center justify-content-center" class="col-12 col-md d-flex align-items-center justify-content-center">
+                        <img src="<?php echo get_template_directory_uri();?>/assets/img/ilami-program.jpg" class="img-fluid" alt="">
+                     </div>
+                     <div class="col-12 col-md d-flex align-items-center justify-content-center" class="col-12 col-md d-flex align-items-center justify-content-center">
+                        <img src="<?php echo get_template_directory_uri();?>/assets/img/ilami-banner-2.webp" class="img-fluid" alt="">
+                     </div>
+                     <div class="col-12 col-md d-flex align-items-center justify-content-center">
+                        <img src="<?php echo get_template_directory_uri();?>/assets/img/ilami.jpg" class="img-fluid" alt="">  
+                     </div>
+                  </div>
+               </div>
+               <div class="carousel-item">
+                  <div class="row">
+                     <div class="col-12 col-md d-flex align-items-center justify-content-center">
+                        <img src="<?php echo get_template_directory_uri();?>/assets/img/logo.png" class="img-fluid" alt="">
+                     </div>
+                     <div class="col-12 col-md d-flex align-items-center justify-content-center">
+                        <img src="<?php echo get_template_directory_uri();?>/assets/img/ilami.jpg" class="img-fluid" alt="">  
+                     </div>
+                     <div class="col-12 col-md d-flex align-items-center justify-content-center" class="col-12 col-md d-flex align-items-center justify-content-center">
+                        <img src="<?php echo get_template_directory_uri();?>/assets/img/ilami-banner.jpg" class="img-fluid" alt="">
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <!--End carousel-->
+      </div>
+      <div class="col-1 d-flex align-items-center justify-content-center">
+        <a role="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <div class="carousel-nav-icon"> <i class="bi bi-arrow-right"></i></div>
+         </a>
+      </div>
+   </div>
+                 </div>
+           </div> 
+       </div> 
+    </div>          
+</section>
+<section id="event-video">
+    <div class="container">
+    <div class="d-flex justify-content-center">
+       <img src="<?php echo get_template_directory_uri();?>/assets/img/ilami-banner-2.webp" width="650px"  alt="">
+       <img src="<?php echo get_template_directory_uri();?>/assets/img/ilami-flyer.jpg" width="650px"  alt="">
+     </div>
+               <div class="embed-responsive embed-responsive-16by9 d-flex justify-content-center mt-5" id="youtube-vid">
+                   <iframe width="850px" height="450" src="https://www.youtube.com/embed/_c23FGU1Cao" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+               </div>
+    </div>
+</section>
+<section id="contact-info">
+    <div class="container">
+        <div class="row">
+                <h3>Contact information:</h3>
+                 <p>For further information kindly contact our representative at your convenience:</p>
+            <div class="col-lg-6">
+                 <h4>For Nepal:</h4>
+                 <p>9802815798 - Chintamani Sharma (Lumbini, Karnali, Sudurpaschim Area)</p>
+                 <p>9802815794 - Dharma Raj Bhandari (Gandaki Province)</p>
+                 <p>9802816969 - Prem Giri (Bagmati, Madhesh and Province 1)</p>
+            </div>
+             <div class="col-lg-6">
+                 <h4>For Foreign Countries: </h4>
+                 <p>00971504823072- Krishna Bahadur Dhakal (Asia Continent)</p>
+                 <p>00610431443794- Santosh Adhikari (Australia and Africa Continent)</p>
+             </div>
+             </div> 
+    </div>
+</section>
+<?php get_footer();?>
